02b83e638f47f284904a0b43b70b82fcb45d514b
Tausif K <mr.takazi.25@gmail.com>
2025-11-21 13:55:02 +0000

    station model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,13 @@
+WITH BIKE AS(
+
+    Select 
+    distinct
+    START_STATIO_ID AS station_id,
+    START_STATION_NAME as station_name,
+    START_LAT as station_lat,
+    START_LNG as station_lng
+    from {{ source('demo', 'bike') }}
+    where RIDE_ID != 'ride_id'
+)
+
+select * from BIKE
